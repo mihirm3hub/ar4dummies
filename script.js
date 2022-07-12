16946e65b0b92f95ba08cc45e67c5591aec1bacb
Glitch (glitch-hello-website) <none>
2022-07-12 16:45:24 +0000

    🛰🗾 Checkpoint ./script.js:32900823/31483 ./index.html:32900823/32

--- script.js
+++ script.js
@@ -14,10 +14,18 @@ Make the "Click me!" button move when the visitor clicks it:
 const btn = document.querySelector("#qrBtn"); // Get the button from the page
 // Detect clicks on the button
 if (btn) {
-  btn.onclick = function() {
-    console.log('clicked')
-    
+  btn.onclick = function () {
+    console.log("clicked");
   };
+}
+
+const emoji = document.querySelector("#emoji");
+if (emoji) {
+  const getRandomEmoji = () => {
+    const emojis = ['😀','😃','😄','😁','😆','😅','🤣','😂','🙂','🙃','😉','😊','😇','🥰','😍','🤩','😘','😗','😚','😙','😋','😛','😜','🤪','😝','🤑','🤗','🤭','🤫','🤔','🤐','🤨','😐','😑','😶','😏','😒','🙄','😬','🤥','😌','😔','😪','🤤','😴','😷','🤒','🤕','🤢','🤮','🤧','🥵','🥶','🥴','😵','🤯','🤠','🥳','😎','🤓','🧐','😕','😟','🙁','😮','😯','😲','😳','🥺','😦','😧','😨','😰','😥','😢','😭','😱','😖','😣','😞','😓','😩','😫','🥱','😤','😡','😠','🤬','😈','👿','💀']
+    emoji.innerHTML = emojis[~~(Math.random() * emojis.length)];
+  };
+  setInterval(getRandomEmoji, 1000);
 }
 // This is a single line JS comment
 /*
