89b1a94b7a1db0efd0104538d5bd673f56c876b2
mihir <mihirmainkarm3@gmail.com>
2022-07-13 10:36:05 +0530

    feat ✨: Landing page

---
+++ app/script.js
@@ -0,0 +1,34 @@
+/*
+This is your site JavaScript code - you can add interactivity and carry out processing
+- Initially the JS writes a message to the console, and moves a button you can add from the README
+*/
+
+// Print a message in the browser's dev tools console each time the page loads
+// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+console.log("Hello 🌎");
+
+/* 
+Make the "Click me!" button move when the visitor clicks it:
+- First add the button to the page by following the "Next steps" in the README
+*/
+const btn = document.querySelector("#qrBtn"); // Get the button from the page
+// Detect clicks on the button
+if (btn) {
+  btn.onclick = function () {
+    console.log("clicked");
+  };
+}
+
+const emoji = document.querySelector("#emoji");
+if (emoji) {
+  const getRandomEmoji = () => {
+    const emojis = ['😀','😃','😄','😁','😆','😅','🤣','😂','🙂','🙃','😉','😊','😇','🥰','😍','🤩','😘','😗','😚','😙','😋','😛','😜','🤪','😝','🤑','🤗','🤭','🤫','🤔','🤐','🤨','😐','😑','😶','😏','😒','🙄','😬','🤥','😌','😔','😪','🤤','😴','😷','🤒','🤕','🤢','🤮','🤧','🥵','🥶','🥴','😵','🤯','🤠','🥳','😎','🤓','🧐','😕','😟','🙁','😮','😯','😲','😳','🥺','😦','😧','😨','😰','😥','😢','😭','😱','😖','😣','😞','😓','😩','😫','🥱','😤','😡','😠','🤬','😈','👿','💀']
+    emoji.innerHTML = emojis[~~(Math.random() * emojis.length)];
+  };
+  setInterval(getRandomEmoji, 1000);
+}
+// This is a single line JS comment
+/*
+This is a comment that can span multiple lines 
+- use comments to make your own notes!
+*/
