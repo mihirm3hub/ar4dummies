709bcc5c26402bc86df90f842cd042343c47c421
Glitch (glitch-hello-website) <none>
2022-07-14 06:50:16 +0000

    😇🛳 Checkpoint ./index.html:32900823/516 ./styles/gallery.css:32900823/76 ./script.js:32900823/281

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ This is your site JavaScript code - you can add interactivity and carry out proc
 // Print a message in the browser's dev tools console each time the page loads
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Hello 🌎");
-
 /* 
 Make the "Click me!" button move when the visitor clicks it:
 - First add the button to the page by following the "Next steps" in the README
