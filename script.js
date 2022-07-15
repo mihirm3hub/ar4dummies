79540f5885ee51e667bc6ca0c320beb623eda09c
Glitch (glitch-hello-website) <none>
2022-07-15 16:04:56 +0000

    👼📠 Checkpoint ./.glitch-assets:32900823/368 ./pages/model-viewer.html:32900823/1006 ./styles/style.css:32900823/205 ./script.js:32900823/515

--- script.js
+++ script.js
@@ -44,3 +44,15 @@ This is a comment that can span multiple lines
         script.src = "//mrdoob.github.io/stats.js/build/stats.min.js";
         document.head.appendChild(script);
     })();*/
+
+function addToClipboard() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+}
