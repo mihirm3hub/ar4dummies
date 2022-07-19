50549f03a8725b064d8b86043a99ecfef7c324c7
Glitch (glitch-hello-website) <none>
2022-07-19 17:08:17 +0000

    😎🚩 Checkpoint ./pages/Zappar.html:32900823/23 ./pages/model-viewer.html:32900823/5

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ var mybutton = document.getElementById("hoverBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
