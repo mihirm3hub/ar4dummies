02c090370636d28cf26dbecf2fbc51b994c53d8e
Glitch (glitch-hello-website) <none>
2022-07-19 17:02:39 +0000

    🙋📉 Checkpoint ./styles/model-viewer-style.css:32900823/741 ./pages/model-viewer.html:32900823/71 ./styles/style.css:32900823/10 ./script.js:32900823/6

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ var mybutton = document.getElementById("hoverBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 100) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
